1db3f53c46089efcd20a57a08a0c0461d71d7ffa
Philip Delorenzo <git@deloarts.com>
2024-08-30 10:40:14 +0200

    Version bump

--- app/const.py
+++ app/const.py
@@ -6,7 +6,7 @@ import os
 import secrets
 from pathlib import Path
 
-VERSION = "0.5.0"
+VERSION = "0.6.0"
 NAME = "glados"
 
 # Security
--- tests/test_glados.py
+++ tests/test_glados.py
@@ -2,4 +2,4 @@ from app.const import VERSION
 
 
 def test_version():
-    assert VERSION == "0.5.0"
+    assert VERSION == "0.6.0"
