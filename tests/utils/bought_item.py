b9c5337979e4d576f9403238015cc761de554a69
Philip Delorenzo <git@deloarts.com>
2024-10-11 13:23:21 +0200

    Update tests

--- tests/utils/bought_item.py
+++ tests/utils/bought_item.py
@@ -1,9 +1,9 @@
+from api.schemas.bought_item import BoughtItemCreateWebSchema
+from config import cfg
+from crud.bought_item import crud_bought_item
+from db.models import BoughtItemModel
 from sqlalchemy.orm import Session
 
-from app.api.schemas.bought_item import BoughtItemCreateSchema
-from app.config import cfg
-from app.crud.bought_item import crud_bought_item
-from app.db.models import BoughtItemModel
 from tests.utils.project import get_test_project
 from tests.utils.user import get_test_user
 from tests.utils.utils import random_bought_item_name
@@ -13,7 +13,7 @@ from tests.utils.utils import random_manufacturer
 
 def create_random_item(db: Session, test_fn_name: str | None = None) -> BoughtItemModel:
     user = get_test_user(db)
-    item_in = BoughtItemCreateSchema(
+    item_in = BoughtItemCreateWebSchema(
         project_id=get_test_project(db).id,
         quantity=1,
         unit=cfg.items.bought.units.default,
