93ed55ae636b808f9f3e8b3cdbf27150b8e28f8e
Philip Delorenzo <git@deloarts.com>
2025-02-12 13:46:34 +0100

    Add bought item api tests

--- tests/api/web/bought_items/test_web_api_bought_items__update_bought_item_optional_field.py
+++ tests/api/web/bought_items/test_web_api_bought_items__update_bought_item_optional_field.py
@@ -5,21 +5,16 @@
 from string import Template
 from typing import Dict
 from typing import List
-from typing import Tuple
 
 from api.schemas.bought_item import BoughtItemSchema
 from api.v1.web.endpoints.bought_items import OptionalFieldName
 from config import cfg
-from crud.bought_item import crud_bought_item
-from crud.project import crud_project
 from fastapi.testclient import TestClient
 from httpx import Response
 from locales import lang
 from sqlalchemy.orm import Session
 
 from tests.utils.bought_item import create_random_item
-from tests.utils.project import create_random_project
-from tests.utils.user import get_test_guest_user
 from tests.utils.user import get_test_super_user
 from tests.utils.user import get_test_user
 from tests.utils.utils import random_lower_string
@@ -85,3 +80,96 @@ def test_update_item_optional_field__normal_user(
         assert response.json()[str(field.value).replace("-", "_")] == t_value
 
         assert responseSchema
+
+
+def test_update_item_optional_field__normal_user__value_not_set(
+    client: TestClient, normal_user_token_headers: Dict[str, str], db: Session
+) -> None:
+    # ----------------------------------------------
+    # PREPARATION
+    # ----------------------------------------------
+
+    t_item = create_random_item(db)
+
+    # ----------------------------------------------
+    # METHODS TO TEST
+    # ----------------------------------------------
+
+    for field in OptionalFieldName:
+        response = client.put(
+            UPDATE_ITEM_OPTIONAL_FIELD.substitute(item_id=t_item.id, field_name=field.value),
+            headers=normal_user_token_headers,
+            params={"value": ""},
+        )
+        responseSchema = BoughtItemSchema(**response.json())
+
+        # ----------------------------------------------
+        # VALIDATION
+        # ----------------------------------------------
+
+        assert response
+        assert response.status_code == 200
+        assert response.json()[str(field.value).replace("-", "_")] == ""
+
+        assert responseSchema
+
+
+def test_update_item_optional_field__normal_user__unknown_field(
+    client: TestClient, normal_user_token_headers: Dict[str, str], db: Session
+) -> None:
+    # ----------------------------------------------
+    # PREPARATION
+    # ----------------------------------------------
+
+    t_item = create_random_item(db)
+
+    # ----------------------------------------------
+    # METHODS TO TEST
+    # ----------------------------------------------
+
+    response = client.put(
+        UPDATE_ITEM_OPTIONAL_FIELD.substitute(item_id=t_item.id, field_name="unknown-field"),
+        headers=normal_user_token_headers,
+        params={"value": "Foo Bar"},
+    )
+
+    # ----------------------------------------------
+    # VALIDATION
+    # ----------------------------------------------
+
+    assert response
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["loc"][0] == "path"
+    assert response.json()["detail"][0]["loc"][1] == "field_name"
+
+
+def test_update_item_optional_field__normal_user__of_another_user(
+    client: TestClient, normal_user_token_headers: Dict[str, str], db: Session
+) -> None:
+    # ----------------------------------------------
+    # PREPARATION
+    # ----------------------------------------------
+
+    t_user = get_test_user(db)
+    t_super = get_test_super_user(db)
+    t_item = create_random_item(db, user=t_super)
+    t_value = random_lower_string()
+
+    # ----------------------------------------------
+    # METHODS TO TEST
+    # ----------------------------------------------
+
+    for field in OptionalFieldName:
+        response = client.put(
+            UPDATE_ITEM_OPTIONAL_FIELD.substitute(item_id=t_item.id, field_name=field.value),
+            headers=normal_user_token_headers,
+            params={"value": t_value},
+        )
+
+        # ----------------------------------------------
+        # VALIDATION
+        # ----------------------------------------------
+
+        assert response
+        assert response.status_code == 403
+        assert response.json()["detail"] == lang(t_user).API.BOUGHTITEM.CANNOT_CHANGE_OTHER_USER_ITEM
