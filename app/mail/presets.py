ad979d3d09e5390efb562bc4dcdb7e13e12e5e80
Philip Delorenzo <git@deloarts.com>
2025-02-22 19:35:34 +0100

    Add fallback templates for email notifications and enhance error logging for missing templates

--- app/mail/presets.py
+++ app/mail/presets.py
@@ -14,6 +14,10 @@ from mail.render import render_template
 from mail.send import send_mail
 from multilog import log
 
+SAMPLE_WELCOME = "welcome.sample.j2"
+SAMPLE_DISC_SPACE_WARN = "disc_space_warning.sample.j2"
+SAMPLE_SCHEDULE_ERROR = "schedule_error.sample.j2"
+
 
 class MailPreset:
     """The preset class. Has only static methods."""
@@ -22,7 +26,13 @@ class MailPreset:
     def send_schedule_error(task_name: str, message: str) -> None:
         """Send a schedule error message to the init-systemuser."""
         log.info(f"Sending schedule error email notification to {cfg.init.mail!r}...")
-        body = render_template(template_file=Path(TEMPLATES, cfg.templates.mail_schedule_error), **locals())
+
+        if Path(TEMPLATES, cfg.templates.mail_schedule_error).exists():
+            body = render_template(template_file=Path(TEMPLATES, cfg.templates.mail_schedule_error), **locals())
+        else:
+            log.error(f"Mail template file {cfg.templates.mail_schedule_error!r} not found")
+            body = render_template(template_file=Path(TEMPLATES, SAMPLE_SCHEDULE_ERROR), **locals())
+
         receiver = Receiver(to=[cfg.init.mail])
         mail = Mail(subject="Glados Notification Service", body=body)
         send_mail(receiver=receiver, mail=mail)
@@ -40,10 +50,13 @@ class MailPreset:
     ) -> None:
         """Send a disc space warning message to the init-systemuser."""
         log.info(f"Sending disc space warning email notification to {cfg.init.mail!r}...")
-        body = render_template(
-            template_file=Path(TEMPLATES, cfg.templates.mail_disc_space_warning),
-            **locals(),
-        )
+
+        if Path(TEMPLATES, cfg.templates.mail_disc_space_warning).exists():
+            body = render_template(template_file=Path(TEMPLATES, cfg.templates.mail_disc_space_warning), **locals())
+        else:
+            log.error(f"Mail template file {cfg.templates.mail_disc_space_warning!r} not found")
+            body = render_template(template_file=Path(TEMPLATES, SAMPLE_DISC_SPACE_WARN), **locals())
+
         receiver = Receiver(to=[cfg.init.mail])
         mail = Mail(subject="Glados Notification Service", body=body)
         send_mail(receiver=receiver, mail=mail)
@@ -53,7 +66,13 @@ class MailPreset:
         """Send a welcome mail for new users."""
         local_url = cfg.server.domain
         log.info(f"Sending welcome email to {email!r}...")
-        body = render_template(template_file=Path(TEMPLATES, cfg.templates.mail_welcome), **locals())
+
+        if Path(TEMPLATES, cfg.templates.mail_welcome).exists():
+            body = render_template(template_file=Path(TEMPLATES, cfg.templates.mail_welcome), **locals())
+        else:
+            log.error(f"Mail template file {cfg.templates.mail_welcome!r} not found")
+            body = render_template(template_file=Path(TEMPLATES, SAMPLE_WELCOME), **locals())
+
         receiver = Receiver(to=[email])
         mail = Mail(subject="Glados Notification Service", body=body)
         send_mail(receiver=receiver, mail=mail)
